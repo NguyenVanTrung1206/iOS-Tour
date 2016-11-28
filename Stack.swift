b70fc16f928818fcb957eaf9718a7a0947a47b0f
TrungNguyen <nvtrung1206@gmail.com>
2016-11-28 21:06:24 +0700

    Complete Push Pop Peek without handling Error

--- Stack.swift
+++ Stack.swift
@@ -1,1 +1,52 @@
 // Preparing for coding stack with swift
+
+class Book {
+    var title: String
+    var author: String
+    var releasedYear: String
+    
+    init(_ title: String, _ author: String, _ releasedYear: String){
+        self.title = title
+        self.author = author
+        self.releasedYear = releasedYear
+    }
+}
+
+class Stack<T> {
+    var list = [T]()
+    
+    func push(_ node: T) {
+        list.insert(node, at: 0)
+    }
+    
+    func pop() -> T {   
+        return list.remove(at: 0)    
+    }
+    
+    func peek() -> T {
+        return list[0]
+    }
+    
+    func display() {
+        print(list)
+        print("----------------")
+    }
+}
+
+var stackInt = Stack<Int>()
+stackInt.display()
+
+stackInt.push(5)
+stackInt.display()
+
+print(stackInt.pop())
+stackInt.display()
+
+
+stackInt.push(6)
+stackInt.push(7)
+stackInt.push(8)
+stackInt.display()
+
+print(stackInt.peek())
+stackInt.display()
